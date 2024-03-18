5370f41257ba5777c80be720fbfbf19c302dac94
Elias Neto <netoeac.dev@gmail.com>
2024-03-18 20:50:53 -0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+const btnSubmit = document.querySelectorAll(".btnPrimary")
+const formEbook = document.getElementById("formEbook")
+
+formEbook.addEventListener('submit', (e)=>{
+    e.preventDefault();
+
+    const email = document.getElementById("email").value;
+    const phone = document.getElementById("phone").value;
+
+
+})
